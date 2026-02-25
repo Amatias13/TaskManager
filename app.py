0efed6fef14cce41863d53b3ecbcc76a4b463efa
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2026-02-25 18:47:51 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+# Caminho absoluto para a base de dados
+db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'database', 'tarefas.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+db = SQLAlchemy(app)
+
+class Tarefa(db.Model):
+    __tablename__ = "tarefas"
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.String(200))
+    feita = db.Column(db.Boolean)
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+@app.route('/')
+def home():
+    todas_as_tarefas = Tarefa.query.all()  # Consultamos e armazenamos todas as tarefas da base de dados
+    # Agora na variável todas_as_tarefas estão armazenadas todas as tarefas.Vamos entregar esta variável ao template index.html
+    return render_template("index.html", lista_de_tarefas=todas_as_tarefas)  # Carrega - se o template index.html
+
+@app.route('/criar-tarefa', methods=['POST'])
+def criar():
+    # tarefa é um objeto da classe Tarefa (uma instância da classe)
+    tarefa = Tarefa(conteudo=request.form['conteúdo_tarefa'], feita=False) # id não é necessário atribuí-lo manualmente, porque a primary key gera-se automaticamente
+    db.session.add(tarefa) # Adicionar o objeto da Tarefa à base de dados
+    db.session.commit() # Executar a operação pendente da base de dados
+    return redirect(url_for('home')) # Redireciona-nos à função home()
+
+@app.route('/eliminar-tarefa/<id>')
+def eliminar(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).delete() # Pesquisa-se dentro da base de dados, aquele registro cujo id coincida com o proporcionado pelo parâmetro da rota. Quando se encontrar elimina-se
+    db.session.commit() # Executar a operação pendente da base de dados
+    return redirect(url_for('home')) # Redireciona-nos à função home() e se tudo correu bem, ao atualizar, a tarefa eliminada não vai aparecer na listagem
+
+@app.route('/tarefa-feita/<id>')
+def feita(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).first() # Obtém-se a tarefa que se procura
+    tarefa.feita = not(tarefa.feita) # Guardar na variável booleana da tarefa, o seu contrário
+    db.session.commit() # Executar a operação pendente da base de dados
+    return redirect(url_for('home')) # Redireciona-nos para a função home()
+
+if __name__ == '__main__':
+    app.run(debug=True)
